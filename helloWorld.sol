29be0dcd2ec924c8b53559ba1f1bfec1ebbe803f
oneworldcoder <52066039+oneworldcoder@users.noreply.github.com>
2020-10-16 11:22:59 +0530

    Create helloWorld.sol

---
+++ helloWorld.sol
@@ -0,0 +1,15 @@
+pragma solidity 0.5.12;
+
+contract HelloWorld {
+    string public message = "Hello World";
+
+    function getMessage() public view returns(string memory){
+        return message;
+    }
+    
+    // To set a new message
+    
+    function setMessage(string memory newMessage) public {
+        message = newMessage;
+    }
+}
